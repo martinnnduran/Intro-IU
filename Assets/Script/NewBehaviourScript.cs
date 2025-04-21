86d021bcef8cdc3347512b4124f73674b8c00b87
martinnnduran <167240751+martinnnduran@users.noreply.github.com>
2025-04-21 12:05:16 -0300

    Intro IU

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+    string saludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saludar()
+    {
+        //Obtener el nombre del inputfield
+
+        nombreIngresado = inputNombre.text;
+
+        //Concatenar el nombre con el saludo
+
+        txtSaludo.text = "Hola " + nombreIngresado + "!";
+
+        //Mostrar el saludo en txtSaludo
+
+        txtSaludo.text = saludo;
+
+    }
+}
